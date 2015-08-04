048e96ff4542cb9a153867fa9e3ddc286fb18fce
Islam Azab <islam.3azab@gmail.com>
2015-08-05 01:19:41 +0200

    fix for comparison when there is no results from geocoder for user input fields

--- lib/utils.rb
+++ lib/utils.rb
@@ -2,15 +2,27 @@ class Utils
   def self.compare_places(place_a, place_b)
      a = Geocoder.search(place_a[:address]).first
      b = Geocoder.search(place_b[:address]).first
+
      response = {}
-     response[:street_number] = a.street_number == b.street_number
-     response[:street_address] = a.street_address == b.street_address
-     response[:postal_code] = a.postal_code == b.postal_code
-     response[:country] = a.country_code == b.country_code
-     response[:state] = a.state == b.state
-     response[:city] = a.city == b.city
-     response[:phone] = place_a[:phone] == place_b[:phone]
-     response[:matching_count] = response.values.keep_if { |e| e }.count
+     if b
+       response[:street_number] = a.street_number == b.street_number
+       response[:street_address] = a.street_address == b.street_address
+       response[:postal_code] = a.postal_code == b.postal_code
+       response[:country] = a.country_code == b.country_code
+       response[:state] = a.state == b.state
+       response[:city] = a.city == b.city
+       response[:phone] = place_a[:phone] == place_b[:phone]
+       response[:matching_count] = response.values.keep_if { |e| e }.count
+     else
+       response[:street_number] = false
+       response[:street_address] = false
+       response[:postal_code] = false
+       response[:country] = false
+       response[:state] = false
+       response[:city] = false
+       response[:phone] = false
+       response[:matching_count] = 0
+     end
 
      response
   end
