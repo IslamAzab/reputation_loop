c57bd8070b05d5460ec8eee85c345ea869d520c9
Islam Azab <islam.3azab@gmail.com>
2015-08-05 13:20:28 +0200

    return comparison of address and name too, todo recheck

--- lib/utils.rb
+++ lib/utils.rb
@@ -9,23 +9,27 @@ class Utils
 
     response = {}
     if a && b
-       response[:street_number] = a.street_number == b.street_number
-       response[:street_address] = a.street_address == b.street_address
-       response[:postal_code] = a.postal_code == b.postal_code
-       response[:country] = a.country_code == b.country_code
-       response[:state] = a.state == b.state
-       response[:city] = a.city == b.city
-       response[:phone] = place_a[:phone] == place_b[:phone]
-       response[:matching_count] = response.values.select { |e| e }.count
+      response[:name] = a.address_components.first["short_name"] == b.address_components.first["short_name"]
+      response[:street_number] = a.street_number == b.street_number
+      response[:street_address] = a.street_address == b.street_address
+      response[:postal_code] = a.postal_code == b.postal_code
+      response[:country] = a.country_code == b.country_code
+      response[:state] = a.state == b.state
+      response[:city] = a.city == b.city
+      response[:phone] = place_a[:phone] == place_b[:phone]
+      response[:address] = place_a[:address] == place_b[:address]
+      response[:matching_count] = response.values.select { |e| e }.count
     else
-       response[:street_number] = false
-       response[:street_address] = false
-       response[:postal_code] = false
-       response[:country] = false
-       response[:state] = false
-       response[:city] = false
-       response[:phone] = false
-       response[:matching_count] = 0
+      response[:name] = false
+      response[:street_number] = false
+      response[:street_address] = false
+      response[:postal_code] = false
+      response[:country] = false
+      response[:state] = false
+      response[:city] = false
+      response[:phone] = false
+      response[:address] = false
+      response[:matching_count] = 0
     end
 
     response
